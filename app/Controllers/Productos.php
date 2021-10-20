9e4c3966465f1a37a99bd4c331629c0949f333a8
John Alexander Cifuentes <john.alexcz@gmail.com>
2021-10-20 08:34:11 -0500

    empezamos a trabajar con el modelo

--- app/Controllers/Productos.php
+++ app/Controllers/Productos.php
@@ -12,23 +12,27 @@ class Productos extends BaseController
     public function registrar()
     {
         //1. recibo todos los datos enviados desde el formulario
-        $producto=$this->request->getPost("producto");
-        $fotografia=$this->request->getPost("fotografia");
-        $precio=$this->request->getPost("precio");
-        $descripcion=$this->request->getPost("descripcion");
-        $tipo=$this->request->getPost("tipo");
+        $producto = $this->request->getPost("producto");
+        $fotografia = $this->request->getPost("fotografia");
+        $precio = $this->request->getPost("precio");
+        $descripcion = $this->request->getPost("descripcion");
+        $tipo = $this->request->getPost("tipo");
 
         //2. Vamos a validar los campos
-        if($this->validate('producto')){
+        if ($this->validate('producto')) {
 
-            //echo("todo esta bien");
-
-        }else{
-
-            $mensaje="Tienes datos pendientes";
-            return redirect()->to(site_url('/productos/registros'))->with('mensaje',$mensaje);
-            //echo("tienes datos pendientes");
-
+            $datos = array(
+                "producto" => $producto,
+                "fotografia" => $fotografia,
+                "precio" => $precio,
+                "descripcion" => $descripcion,
+                "tipo" => $tipo
+            );
+            print_r($datos);
+            
+        } else {
+            $mensaje = "Tienes datos pendientes";
+            return redirect()->to(site_url('/productos/registros'))->with('mensaje', $mensaje);
         }
 
         //3. crear un arreglo asocialivo con los datos del punto 1
